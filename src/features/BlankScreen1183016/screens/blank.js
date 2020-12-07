d39565f92ef0b8bc102a95f7875a14541b3fc172
Crowdbotics <team@crowdbotics.com>
2020-12-07 18:47:48 +0000

    Update components

--- src/features/BlankScreen1183016/screens/blank.js
+++ src/features/BlankScreen1183016/screens/blank.js
@@ -44,6 +44,7 @@ export class Blank extends React.Component {
               uri:
                 "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/nike_1SyIE6r.jpg"
             }}
+            style={styles.Image_15}
           />
         </View>
         <View style={styles.View_13}>
@@ -66,11 +67,21 @@ const styles = StyleSheet.create({
   View_3: { width: "91%", height: 0, alignItems: "center" },
   Image_5: {},
   TextInput_7: {},
-  View_11: { width: "92%", flexDirection: "row" },
-  View_12: { width: "50%", alignSelf: "center", alignContent: "center" },
-  Image_15: {},
-  View_13: { width: "50%", alignSelf: "center", alignItems: "center" },
-  Image_19: { alignSelf: "center" }
+  View_11: { width: "92%", height: "0%", flexDirection: "row" },
+  View_12: {
+    width: "50%",
+    height: 50,
+    alignSelf: "center",
+    alignContent: "center"
+  },
+  Image_15: { height: 50 },
+  View_13: {
+    width: "50%",
+    height: 50,
+    alignSelf: "center",
+    alignItems: "center"
+  },
+  Image_19: { height: 50, alignSelf: "center" }
 })
 
 function mapStateToProps(state) {
